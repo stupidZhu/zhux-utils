3d8a45f4c276cccabe00e114bea90ec62114e488
zhuzhu <1606017097@qq.com>
2022-09-11 21:06:02 +0800

    build: init project

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,7 @@
-module.exports = {
+export default {
   extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
-};
+}
